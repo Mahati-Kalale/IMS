19fcff8ea6aa994d8dd64c1cf3b5d64af57961ed
Mahati-Kalale <mahati.kalale@gmail.com>
2023-07-20 09:27:03 +0530

    Create User account

--- users-add.php
+++ users-add.php
@@ -1,6 +1,7 @@
 <?php
     session_start();
     if(!isset($_SESSION['user'])) header('location: login.php');
+    $_SESSION['table']='users';
     $user=$_SESSION['user'];
 ?>
 <!DOCTYPE html>
@@ -10,6 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IMS dashboard - Inventory Management System</title>
     <link rel="stylesheet" type="text/css" href="CSS/dashboard.css">
+    <link rel="stylesheet" type="text/css" href="CSS/user-add.css">
     <script src="https://use.fontawesome.com/0c7a3095b5.js"></script>
 </head>
 <body>
@@ -19,25 +21,37 @@
             <?php include('Partials/app-topnav.php')?>
             <div class="dashboard_content">
                 <div class="dashboard_content_main">
-                    <form action="">
-                        <div>
-                            <label for="first_name">First Name</label>
-                            <input type="text" id="first_name" name="first_name"/>
-                        </div>
-                        <div>
-                            <label for="last_name">Last Name</label>
-                            <input type="text" id="last_name" name="last_name"/>
-                        </div>
-                        <div>
-                            <label for="email">Email</label>
-                            <input type="text" id="email" name="email"/>
-                        </div>
-                        <div>
-                            <label for="password">Password</label>
-                            <input type="password" id="password" name="password"/>
-                        </div>
-                        <button type="submit"><i class="fa fa-plus"></i>Add User</button>
-                    </form>
+                    <div id="userAddFormContainer">
+                        <form action="Database/add.php" method = "POST" class="appForm">
+                            <div class="appFormInputContainer">
+                                <label for="first_name">First Name</label>
+                                <input type="text" class="appFormInput" id="first_name" name="first_name"/>
+                            </div>
+                            <div class="appFormInputContainer">
+                                <label for="last_name">Last Name</label>
+                                <input type="text" class="appFormInput" id="last_name" name="last_name"/>
+                            </div>
+                            <div class="appFormInputContainer">
+                                <label for="email">Email</label>
+                                <input type="text" class="appFormInput" id="email" name="email"/>
+                            </div>
+                            <div class="appFormInputContainer">
+                                <label for="password">Password</label>
+                                <input type="password" class="appFormInput" id="password" name="password"/>
+                            </div>
+                            <button type="submit" class="appBtn"><i class="fa fa-plus"></i>Add User</button>
+                        </form>
+                        <?php if(isset($_SESSION['response'])){ 
+                                $response_message=$_SESSION['response']['message'];
+                                $is_success=$_SESSION['response']['success']; 
+                            ?>
+                            <div class="responseMessage">
+                                <p class="<?= $is_success ? 'responseMessage__success' : 'responseMessage__error' ?>">
+                                    <?= $response_message?>
+                                </p>
+                            </div>
+                        <?php unset($_SESSION['response']); } ?>
+                    </div>
                 </div>
             </div>
         </div> 
